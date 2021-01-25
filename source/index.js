299094734630066cd3864222860c16216107bec0
NagaVlad <nagayychenkovladislav@gmail.com>
2021-01-25 20:34:48 +0300

    up2

--- source/index.js
+++ source/index.js
@@ -45,14 +45,14 @@ document.querySelector('#back').addEventListener('click', (e) => {
 })
 
 async function main() {
-   let location = await fetch(`https://flamboyant-rosalind-326379.netlify.app/https://ipinfo.io/json?token=${token}`)
+   let location = await fetch(`https://ipinfo.io/json?token=${token}`)
       .then(
          (response) => response.json()
       ).then(
          (jsonResponse) => jsonResponse
       )
 
-   let newCity2 = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=08d6846f461f4edaa9a91251212101&q=59.9386,30.3141&days=3/https://flamboyant-rosalind-326379.netlify.app`)
+   let newCity2 = await fetch(`https://flamboyant-rosalind-326379.netlify.app/https://api.opencagedata.com/geocode/v1/json?q=${location.city}&key=2d4dd65ac76a49cd8dffa74cab0fd692&pretty=1&no_annotations=1`)
       .then(
          (response) => response.json()
       ).then(
