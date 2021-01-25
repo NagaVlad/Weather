f0d2cf70a76fd2df28528592ca3c7fd3ccbc19e6
NagaVlad <nagayychenkovladislav@gmail.com>
2021-01-25 21:38:05 +0300

    up2

--- source/index.js
+++ source/index.js
@@ -46,7 +46,7 @@ document.querySelector('#back').addEventListener('click', (e) => {
 
 async function main() {
    http://api.weatherapi.com/v1/forecast.json?key=08d6846f461f4edaa9a91251212101&q=59.9386,30.3141&days=3/https://flamboyant-rosalind-326379.netlify.app
-   let location = await fetch(URL(`http://api.weatherapi.com/v1/forecast.json?key=08d6846f461f4edaa9a91251212101&q=59.9386,30.3141&days=3/https://flamboyant-rosalind-326379.netlify.app`)
+   let location = await fetch(`../../../../http://api.weatherapi.com/v1/forecast.json?key=08d6846f461f4edaa9a91251212101&q=59.9386,30.3141&days=3`)
       // let location = await fetch(`flamboyant-rosalind-326379.netlify.apphttps://ipinfo.io/json?token=efca0f8ab010de`)
       .then(
          (response) => response.json()
