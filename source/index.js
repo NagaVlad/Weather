f368b509115f441122254d2a8e77c145174633e0
NagaVlad <nagayychenkovladislav@gmail.com>
2021-01-25 21:08:24 +0300

    up

--- source/index.js
+++ source/index.js
@@ -45,7 +45,7 @@ document.querySelector('#back').addEventListener('click', (e) => {
 })
 
 async function main() {
-   let location = await fetch(`https://ipinfo.io/json?token=efca0f8ab010de/https://flamboyant-rosalind-326379.netlify.app`)
+   let location = await fetch(`https://ipinfo.io/json?token=${token}`)
       .then(
          (response) => response.json()
       ).then(
