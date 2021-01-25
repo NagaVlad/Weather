a71173ad4bb8d8674433cae32c1de0bfac7bde83
NagaVlad <nagayychenkovladislav@gmail.com>
2021-01-25 20:12:05 +0300

    upload2

--- source/index.js
+++ source/index.js
@@ -256,6 +256,7 @@ init2()
 
 async function newCity(name) {
    let seacrhGeo = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${name}&key=2d4dd65ac76a49cd8dffa74cab0fd692&pretty=1&no_annotations=1/https://flamboyant-rosalind-326379.netlify.app`)
+
       .then(
          (response) => response.json()
       ).then(
@@ -264,6 +265,7 @@ async function newCity(name) {
    q = `${seacrhGeo.results[0].geometry.lat},${seacrhGeo.results[0].geometry.lng}`
    qCity = `${seacrhGeo.results[0].components.city}`
    return seacrhGeo
+   console.log('');
 }
 
 async function ip() {
